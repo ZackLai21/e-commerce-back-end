52b55516ae286c0f05e09835a6165455b2555113
Zeyang Lai <decyctus@gmail.com>
2021-10-28 22:36:21 -0700

    added some comments

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async(req, res) => {
   // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({include:[{model:Product,through:ProductTag}]});
     res.status(200).json(tagData);
@@ -16,7 +15,6 @@ router.get('/', async(req, res) => {
 
 router.get('/:id', async(req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id,{include:[{model:Product,through:ProductTag}]});
 
