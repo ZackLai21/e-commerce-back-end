fb825840a178c48aaa038d6f72a65ca3e8ed5d4b
Zeyang Lai <decyctus@gmail.com>
2021-10-28 18:35:52 -0700

    finished the product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({include:[{model:Category},{model:Tag,through:ProductTag,as:"tag"}]});
+    const productData = await Product.findByPk(req.params.id,{include:[{model:Category},{model:Tag,through:ProductTag,as:"tag"}]});
 
     if (!productData) {
       res.status(404).json({ message: "No product found with this id!" });
@@ -107,8 +107,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
